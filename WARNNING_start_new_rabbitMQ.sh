56be4119ed4da5dbf2cbfe9c465ee2e61e894d9b
DF <a2470abc@gmail.com>
2021-07-30 16:23:18 +0800

    rename to be sure want u do

--- WARNNING_start_new_rabbitMQ.sh
+++ WARNNING_start_new_rabbitMQ.sh
@@ -1,10 +1,10 @@
 sudo docker-compose down
 sudo docker-compose rm
 sudo rm -rf data
-echo "removed data"
+echo "removed data" | lolcat
 # make a dir to save persisten data with open permission
 mkdir data 
-echo "created data"
+echo "created data" | lolcat
 sudo chmod 777 data
-echo "chmod done"
+echo "chmod done" | lolcat
 sudo docker-compose up -d
